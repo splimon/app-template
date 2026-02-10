ee4d8ed61beabc0d4aabf14239651835a3b1f17a
Jaden <jaden.kapali@gmail.com>
2026-02-10 10:02:19 -1000

    update jest configuration to correct setup file path and change test environment to node

--- jest.config.ts
+++ jest.config.ts
@@ -145,7 +145,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ['./tests/setup.tsx'],
+  setupFilesAfterEnv: ['./src/tests/setup.tsx'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -154,7 +154,7 @@ const config: Config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  testEnvironment: "node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
